606ca37ba643c156884796af2999a11972197180
socialwing02 <socialwing02@gmail.com>
2025-01-03 12:05:45 +0530

    hotel

--- dubai_webiste/duabi_website/src/app/hotel/page.tsx
+++ dubai_webiste/duabi_website/src/app/hotel/page.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import Hotel from "../../components/Hotel/Hotel-p-1";
+import HotelCard from "../../components/Hotel/Hotel-Card";
+import Hotel1stcard from "../../components/Hotel/hotel1stcard";
+import Standchef from '../../components/Hotel/Standchef';
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <div>
+      <Hotel1stcard/>
+      <Hotel />
+      <HotelCard/>
+      <Standchef/>
+      
+    </div>
+  );
 }
